68496c6e3363a31997091b3cde8adcf7861cb603
David James <davidbjames1@gmail.com>
2015-08-27 15:41:01 +0200

    Created Patterns project

---
+++ Patterns/Patterns.h
@@ -0,0 +1,19 @@
+//
+//  Patterns.h
+//  Patterns
+//
+//  Created by David James on 8/27/15.
+//  Copyright © 2015 Chaotic Moon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Patterns.
+FOUNDATION_EXPORT double PatternsVersionNumber;
+
+//! Project version string for Patterns.
+FOUNDATION_EXPORT const unsigned char PatternsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Patterns/PublicHeader.h>
+
+
